8bd032504345febce329515a5df46ea41a720b5c
kalantariamin1369-ux <kalantari.amin1369@gmail.com>
2025-10-26 10:51:03 +0330

    Add main form UI and assembly information

---
+++ EquipmentTracker/MainForm.Designer.cs
@@ -0,0 +1,21 @@
+namespace EquipmentTracker
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        // This method is required for the designer support.
+        // The contents are initialized in the InitializeComponent method in MainForm.cs
+        #endregion
+    }
+}
---
+++ EquipmentTracker/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Equipment Tracker")]
+[assembly: AssemblyDescription("A simple equipment inventory management application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Equipment Tracker")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f0c0f0c0-f0c0-f0c0-f0c0-f0c0f0c0f0c0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
